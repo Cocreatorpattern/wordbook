71c865c573f2001c815b1afd55db4567dac6d562
Cocreatorpattern <136176709+Cocreatorpattern@users.noreply.github.com>
2023-11-17 15:22:41 +0000

    added api key

--- src/Wordbook.js
+++ src/Wordbook.js
@@ -1,28 +1,31 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./Wordbook.css";
 import axios from "axios";
 
+export default function Wordbook() {
+  let [keyword, setkeyword] = useState("");
 
-export default function Wordbook () {
-let [keyword, setkeyword] = useState("");
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
 
-function search(event){
-event.preventDefault();
-alert(`searching for ${keyword} definition`);
+  function search(event) {
+    event.preventDefault();
+    alert(`searching for ${keyword} definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setkeyword(event.target.value);
+  }
+
+  return (
+    <div className="Wordbook">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
 }
-
-function handlekeywordchange (event) {
-setkeyword(event.target.value);
-}
-
-return ( 
-<div className= "Wordbook" > 
-<form onSubmit={search}>
-    <input type="search" onChange={handlekeywordchange}/>
-     </form>
-    
-</div>
-
-);
-}
-
