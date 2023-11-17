04855ec0cc5be96b155e23fbc1560884242ace03
Cocreatorpattern <136176709+Cocreatorpattern@users.noreply.github.com>
2023-11-17 23:09:53 +0000

    added github addres

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import Wordbook from "./Wordbook";
-
 
 export default function App() {
   return (
     <div className="App">
-    <div className="container">
-    
-    <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <main> 
-         <Wordbook/>
-      </main>
-      <footer className="App-footer"> 
-      <small> coded by Nneka Umerah  </small>
-
-      </footer>
-       </header> 
-        
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <main>
+            <Wordbook />
+          </main>
+          <footer className="App-footer">
+            <small> coded by Nneka Umerah </small>
+            and is{" "}
+            <a href="https://github.com/Cocreatorpattern/wordbook">
+              open sourced on Github
+            </a>
+          </footer>
+        </header>
+      </div>
     </div>
-    </div>
-    
   );
 }
-
